7e941a906366d7c137c77186af1d695d8182e419
Robert C. Duvall <robertcduvall@gmail.com>
2015-02-16 13:56:01 -0500

    Converted ALL loops to use new Java 8 features.

--- src/TagCloud.java
+++ src/TagCloud.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 /**
@@ -27,8 +29,10 @@ public class TagCloud {
     private static final int DEFAULT_MIN_FONT = 6;
     private static final int DEFAULT_INCREMENT = 4;
     private static final String DEFAULT_IGNORE_FILE = "common.txt";
-    // regular expression that represents one or more digits or punctuation
+    // key regular expressions
     private static final String PUNCTUATION = "[\\d\\p{Punct}]+";
+    private static final String END_OF_FILE = "\\Z";
+    private static final String WHITESPACE = "\\s";
 
     // set of common words to ignore when displaying tag cloud
     private Set<String> myCommonWords;
@@ -40,62 +44,17 @@ public class TagCloud {
      * Constructs an empty TagCloud.
      */
     public TagCloud (Scanner ignoreWords) {
+        // this value should never be null
         myTagWords = new ArrayList<>();
-        myCommonWords = new HashSet<>();
         // create list of words that should not be included in final word counts
-        while (ignoreWords.hasNext()) {
-            myCommonWords.add(sanitize(ignoreWords.next()));
-        }
+        myCommonWords = new HashSet<>(readWords(ignoreWords));
     }
 
     /**
-     * Reads given text file and counts non-common words it contains.
-     *
-     * Each word read is converted to lower case with leading and trailing punctuation removed
-     * before it is counted.
+     * Create a word cloud from the given input.
      */
-    public void countWords (Scanner input) {
-        Map<String, Integer> wordCounts = new HashMap<>();
-        while (input.hasNext()) {
-            String word = sanitize(input.next());
-            if (isTaggable(word)) {
-                if (wordCounts.containsKey(word)) {
-                    wordCounts.put(word, wordCounts.get(word) + 1);
-                }
-                else {
-                    wordCounts.put(word, 1);
-                }
-            }
-        }
-        myTagWords.addAll(wordCounts.entrySet());
-    }
-
-    /**
-     * Sorts words alphabetically, keeping only those that appeared most often.
-     */
-    public void topWords (int numWordsToKeep, int groupSize) {
-        // sort from most frequent to least
-        myTagWords.sort(new Comparator<Entry<String, Integer>>() {
-            @Override
-            public int compare (Entry<String, Integer> a, Entry<String, Integer> b) {
-                return b.getValue().compareTo(a.getValue());
-            }
-        });
-        // keep only the top ones
-        myTagWords.subList(numWordsToKeep, myTagWords.size()).clear();
-        // convert frequencies into groups
-        for (int k = 0; k < myTagWords.size(); k++) {
-            Entry<String, Integer> word = myTagWords.get(k);
-            myTagWords.set(k,
-                           new SimpleEntry<String, Integer>(word.getKey(), word.getValue() / groupSize));
-        }
-        // sort alphabetically
-        myTagWords.sort(new Comparator<Entry<String, Integer>>() {
-            @Override
-            public int compare (Entry<String, Integer> a, Entry<String, Integer> b) {
-                return a.getKey().compareTo(b.getKey());
-            }
-        });
+    public void makeCloud (Scanner input, int numWordsToKeep, int groupSize) {
+        myTagWords = topWords(countWords(input), numWordsToKeep, groupSize);
     }
 
     /**
@@ -103,13 +62,42 @@ public class TagCloud {
      */
     @Override
     public String toString () {
-        StringBuilder result = new StringBuilder();
-        result.append(HTMLPage.startPage(DEFAULT_NUM_GROUPS, DEFAULT_MIN_FONT, DEFAULT_INCREMENT));
-        for (Entry<String, Integer> word : myTagWords) {
-            result.append(HTMLPage.formatWord(word.getKey(), word.getValue()));
-        }
-        result.append(HTMLPage.endPage());
-        return result.toString();
+        String words = myTagWords.stream()
+                                 .map(HTMLPage::formatWord)
+                                 .collect(Collectors.joining(" "));
+        return HTMLPage.startPage(DEFAULT_NUM_GROUPS, DEFAULT_MIN_FONT, DEFAULT_INCREMENT) + 
+               words +
+               HTMLPage.endPage();
+    }
+
+    // Reads given text file and counts non-common words it contains.
+    // Each word read is converted to lower case with leading and trailing punctuation removed
+    // before it is counted.
+    private List<Entry<String, Integer>> countWords (Scanner input) {
+        final Map<String, Integer> wordCounts = new HashMap<>();
+        readWords(input).forEach(w -> {
+            if (isTaggable(w)) {
+                wordCounts.put(w, wordCounts.getOrDefault(w, 0) + 1);
+            }
+        });
+        return new ArrayList<Entry<String, Integer>>(wordCounts.entrySet());
+    }
+
+    // Sorts words alphabetically, keeping only those that appeared most often.
+    private List<Entry<String, Integer>> topWords (List<Entry<String, Integer>> tagWords,
+                                                   int numWordsToKeep,
+                                                   int groupSize) {
+        // sort from most frequent to least
+        tagWords.sort(Comparator.comparing(Entry<String, Integer>::getValue).reversed());
+        // keep only the top ones
+        tagWords.subList(numWordsToKeep, tagWords.size()).clear();
+        // convert frequencies into groups
+        tagWords = tagWords.stream()
+                           .map(w -> new SimpleEntry<String, Integer>(w.getKey(), w.getValue() / groupSize))
+                           .collect(Collectors.toList());
+        // sort alphabetically
+        tagWords.sort(Comparator.comparing(Entry<String, Integer>::getKey));
+        return tagWords;
     }
 
     // Return true if the given word should be tagged
@@ -117,11 +105,20 @@ public class TagCloud {
         return word.length() > 0 && !myCommonWords.contains(word);
     }
 
-    // Return string with leading and trailing punctuation removed from the given word
-    private String sanitize (String word) {
-        return word.replaceFirst("^" + PUNCTUATION, "").replaceFirst(PUNCTUATION + "$", "")
+    // Remove the leading and trailing punctuation from the given word
+    private static String sanitize (String word) {
+        return word.replaceFirst("^" + PUNCTUATION, "")
+                   .replaceFirst(PUNCTUATION + "$", "")
                    .toLowerCase();
     }
+
+    // Remove the leading and trailing punctuation from the given word
+    private List<String> readWords (Scanner input) {
+        List<String> words = Arrays.asList(input.useDelimiter(END_OF_FILE).next().split(WHITESPACE));
+        return words.stream().map(TagCloud::sanitize)
+                    .collect(Collectors.toList());
+    }
+
 
     public static void main (String[] args) {
         if (args.length == 0) {
@@ -130,9 +127,10 @@ public class TagCloud {
         else {
             try {
                 TagCloud cloud = new TagCloud(
-                                   new Scanner(TagCloud.class.getResourceAsStream(DEFAULT_IGNORE_FILE)));
-                cloud.countWords(new Scanner(new File(args[1])));
-                cloud.topWords(Integer.parseInt(args[0]), DEFAULT_NUM_GROUPS);
+                                     new Scanner(TagCloud.class.getResourceAsStream(DEFAULT_IGNORE_FILE)));
+                cloud.makeCloud(new Scanner(new File(args[1])), 
+                                Integer.parseInt(args[0]),
+                                DEFAULT_NUM_GROUPS);
                 System.out.println(cloud);
             }
             catch (FileNotFoundException e) {
